7be3be42e0be46a1813b0c3ca1c355810fda44bb
giovaniocan <giovaniocan@gmail.com>
2024-03-25 11:11:31 -0300

    added asycnStorage, and loading page

--- src/context/Auth.tsx
+++ src/context/Auth.tsx
@@ -1,6 +1,7 @@
-import { ReactNode, createContext, useState } from "react";
+import { ReactNode, createContext, useEffect, useState } from "react";
 import { authService } from "../services/authService";
 import { Alert } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export interface AuthData{
     token: string
@@ -10,33 +11,54 @@ export interface AuthData{
 
 interface AuthContextType {
     authData?:AuthData
-    signIn:(email:string, password:string) => Promise<AuthData>
+    signIn:(email:string, password:string) => void
     signOut:() => Promise<void>
+    loading: boolean
 }
 
 export const AuthContext = createContext<AuthContextType>({} as AuthContextType)
 
 export const AuthProvider: React.FC<{children:ReactNode}> = ({children}) => {
     const [authData, setAuthData] = useState<AuthData>()
+    const [loading, setLoading] = useState(true)
 
-   async function signIn(email:string, password:string): Promise<AuthData>{
+    async function loadFromStorage(){
+        const auth = await AsyncStorage.getItem('@AuthData') 
+        if(auth){
+            setAuthData(JSON.parse(auth) as AuthData)
+        } 
+        setLoading(false)
+    }
+
+    useEffect(() => {
+        loadFromStorage()
+    }, [])
+
+   async function signIn(email:string, password:string){
        try {
         const auth = await authService.signIn(email, password)
    
         setAuthData(auth)
+        AsyncStorage.setItem('@AuthData', JSON.stringify(auth))
          
         return auth
-       } catch (error) {
-            Alert.alert(error.message, 'Try again')
-       }
+       } catch (error ) {
+            if (error instanceof Error) {
+                Alert.alert(error.message, 'Try again');
+            } else {
+                Alert.alert('An error occurred', 'Try again');
+            }
+            throw error; // Se deseja propagar o erro para o chamador
+        }
     }
 
     async function signOut():Promise<void>{
         setAuthData(undefined)
+        AsyncStorage.removeItem('@AuthData')
         return;
     }
     return(
-        <AuthContext.Provider value={{authData, signIn, signOut}}>
+        <AuthContext.Provider value={{authData, loading, signIn, signOut}}>
             {children}
         </AuthContext.Provider>
     )
--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -2,10 +2,22 @@ import { NavigationContainer } from "@react-navigation/native";
 import { AuthStack } from "./AuthStack";
 import { AppStack } from "./AppStack";
 import { useAuth } from "../hooks/useAuth";
+import { ActivityIndicator, View } from "react-native";
+import { styles } from "../screens/styles";
 
 
 export function Router(){
-    const {authData} = useAuth()
+    const {authData, loading} = useAuth()
+
+    if (loading) {
+        // Exibe o ActivityIndicator enquanto o aplicativo está carregando
+        return (
+            <View style={styles.container}>
+                <ActivityIndicator size="large" color="#0000ff" />
+            </View>
+        );
+    }
+
     return(
         <NavigationContainer >
             {authData ? <AppStack /> : <AuthStack />}
